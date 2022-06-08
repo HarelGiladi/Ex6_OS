d3dc68b3e226e3bf62de9f01865d09127b2cd22b
HarelGiladi <harel495@gmail.com>
2022-06-08 04:07:33 +0300

    .

--- Test.cpp
+++ Test.cpp
@@ -1,12 +1,17 @@
 
-// #include <stdio.h>
-// #include <sys/socket.h>
-// #include <stdlib.h>
-// #include <netinet/in.h>
-// #include <arpa/inet.h>
-// #include <assert.h>
-// #include <string.h>
-// #include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <string.h>
+#include <iostream>
+#include "Queue.hpp"
+#include "AO.hpp"
+#include "reactor.hpp"
+
+
 
 // #include "Mem_Imp.hpp"
 // using namespace Ex5;
@@ -73,3 +78,89 @@
 //     recv(sock, input, 1024, 0);
 //     if (precmp(" end",input)){cout << "TEST COMPLETED SUCCESSFULLY" << endl;}
 // }
+
+
+int main(){
+    Queue *q1 = new Queue();
+    Queue *q2 = new Queue();
+    Queue *q3 = new Queue();
+
+    active_object* ao_1 , *ao_2, *ao_3; 
+    ao_1 = newAO(q1,NULL,NULL);
+    ao_2 = newAO(q1,NULL,NULL);
+    ao_3 = newAO(q1,NULL,NULL);
+
+    reactor *r1 = new reactor();
+    reactor *r2 = new reactor();
+    reactor *r3 = new reactor();
+    
+    char ex6[] = {'e','x','6'};
+    char ex6_2[] = {'e','x','6','2'};
+
+    std::cout <<"started test on queue"<<std::endl;
+
+    assert(q1->size == q2->size);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(q3->size == q2->size);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(q1->size == q3->size);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+
+    q1->enQ(ex6);
+    q1->enQ(ex6_2);
+    q2->enQ(ex6);
+    assert(q1->size == 2);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    q1->deQ();
+    assert(q1->size ==1);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(q1->size == 1);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(q1->size == q2->size);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    q2->deQ();
+    assert(q3->size == q2->size);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    std::cout <<"!TEST PASSED SECCSESFULLY!"<<std::endl;
+
+
+    std::cout <<"test started on AO"<<std::endl;
+    assert(ao_1->run == true);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(ao_2->run == true);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(ao_3->run == true);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(ao_3->run == ao_2->run);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(ao_2->run == ao_1->run);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(ao_1->run == ao_3->run);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    std::cout <<"!TEST PASSED SECCSESFULLY!"<<std::endl;
+
+
+
+    std::cout <<"test started on reactor"<<std::endl;
+    assert(r1->fd_count == r2->fd_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(r3->fd_count == r2->fd_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(r1->fd_count == r3->fd_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+
+    assert(r1->func_count == r3->func_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(r1->func_count == r2->func_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(r2->func_count == r3->func_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+
+    assert(r1->fd_count == r1->func_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    assert(r2->fd_count == r2->func_count);
+    std::cout <<"!ASSERTION WAS A SECCSES!"<<std::endl;
+    std::cout <<"!TEST PASSED SECCSESFULLY!"<<std::endl;
+    std::cout <<"!ALL TESTS PASSED SECCSESFULLY!"<<std::endl;
+        
+}
--- makefile
+++ makefile
@@ -3,21 +3,23 @@ FLAGS=-Wall -pthread
 
 all: Ex6
 
-Ex6: singleton_test.exe guard_test.exe server.exe client.exe select_client.exe pollserver.exe
+Ex6: singleton_test.exe guard_test.exe server.exe client.exe select_client.exe pollserver.exe test.exe
 
 # running programs
-server.exe: server.o Queue.hpp 
+server.exe: server.o Queue.hpp AO.hpp
 	$(CC) $(FLAGS) server.o -o server.exe 
-client.exe: client.o
+client.exe: client.o AO.hpp
 	$(CC) $(FLAGS) -o client.exe client.o
 pollserver.exe: pollserver.o reactor.hpp
 	$(CC) $(FLAGS) pollserver.o -o pollserver.exe 
-select_client.exe: select_client.o reactor.hpp
-	$(CC) $(FLAGS) select_client.o -o select_client.exe
+select_client.exe: selectclient.o reactor.hpp
+	$(CC) $(FLAGS) selectclient.o -o select_client.exe
 singleton_test.exe: singleton.o
 	$(CC) $(FLAGS) singleton.o -o singleton_test.exe
 guard_test.exe: guard.o
 	$(CC) $(FLAGS) guard.o -o guard_test.exe
+test.exe: Test.o reactor.hpp Queue.hpp AO.hpp
+	$(CC) $(FLAGS) Test.o -o test.exe
 
 #object files before linker
 server.o: server.cpp 
@@ -26,12 +28,14 @@ client.o: client.cpp
 	$(CC) $(FLAGS) -c client.cpp
 pollserver.o: pollserver.cpp 
 	$(CC) $(FLAGS) -c pollserver.cpp
-select_client.o: selectclient.cpp 
+selectclient.o: selectclient.cpp 
 	$(CC) $(FLAGS) -c selectclient.cpp
 singleton.o: singleton.cpp
 	$(CC) $(FLAGS) -c singleton.cpp
 guard.o: guard.cpp
 	$(CC) $(FLAGS) -c guard.cpp
+Test.o: Test.cpp
+	$(CC) $(FLAGS) -c Test.cpp
 
 
 .PHONY: clean all
